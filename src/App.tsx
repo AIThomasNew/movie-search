4fa9464e487df8ec412451a8e3f97f81b2424880
Thomas <thomas.new.it@gmail.com>
2022-06-01 12:54:29 +0500

    new branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 const App = () => {
-  return <div className="App">Movie Search</div>
+  // const [film, setFilm] = useState(null)
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch('http://www.omdbapi.com/?apikey=e6c21204=batman')
+        const data = await response.json()
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getData()
+  }, [])
+
+  return (
+    <div className="App">
+      <div>Movie Search </div>
+    </div>
+  )
 }
 
 export default App
