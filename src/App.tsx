c4fb3cf911fe39b9ec6c804426c8b9c544f8bc1a
Danila Alpatov <danila.alpatov@aviasales.ru>
2022-06-01 12:11:19 +0300

    Added tmdb key and request example

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
-import { useEffect, useState } from 'react'
+import {FC, useEffect, useState} from 'react'
 import './App.css'
+import {getMovie} from "./api";
+import {Movie} from "./api/types";
 
 const App = () => {
-  // const [film, setFilm] = useState(null)
+  const [movie, setMovie] = useState<Movie|null>(null)
+  const [movieId, setMovieId] = useState<number|null>(null)
 
   useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await fetch('http://www.omdbapi.com/?apikey=e6c21204=batman')
-        const data = await response.json()
-        console.log(data)
-      } catch (error) {
-        console.log(error)
-      }
+    if(movieId !== null){
+      getMovie(movieId).then(setMovie)
     }
-    getData()
-  }, [])
+  }, [movieId])
 
   return (
     <div className="App">
       <div>Movie Search </div>
+      <input type="text" onChange={e => setMovieId(+e.target.value)} value={movieId || ''}/>
+      {movie && <MovieCard movie={movie}/>}
     </div>
   )
+}
+
+
+const MovieCard: FC<{movie: Movie}> = ({movie}) => {
+  return <h2>{movie.original_title} ({movie.release_date})</h2>
 }
 
 export default App
